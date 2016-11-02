742cb89ad200ecae05779d744658324196664934
Renato <r.budinich@math.uni-goettingen.de>
2016-11-02 16:47:26 +0100

    more work on roi.py. currently broken

--- scripts/roi.py
+++ scripts/roi.py
@@ -13,31 +13,42 @@ rect = (40,90, 110,150)
 labels = set()
 for i in range(rect[0],rect[2]+1):
     for j in range(rect[1],rect[3]+1):
-        labels.add(img.label_img[i,j])
+        labels.add(int(img.label_img[i,j]))
 
+#fig = plt.figure()
+#plt.imshow(img.img,cmap=plt.cm.gray)
+#ax = fig.gca()
+#x = rect[1]
+#y = rect[0]
+#width = rect[3] - rect[1]
+#height = rect[2] - rect[0]
+#ax.add_patch(patches.Rectangle( (x, y),width,height,color = 'red',fill=False))
+#plt.show()
+
+        
 coeffset = set()
 for regionidx in labels:
     region = img.rbepwt.region_collection_at_level[1][int(regionidx)]
-    #region_points = copy.copy(region.points)
-    #for i,j in region_points:
-    #    value = region_points[i,j]
-
-    #invperm = sorted(range(len(region)), key = lambda k: region.permutation[k])
-    #for 
     for level in range(1,img.rbepwt.levels):
         underregion = img.rbepwt.region_collection_at_level[level+1][regionidx]
         invperm = sorted(range(len(underregion)), key = lambda k: underregion.permutation[k])
         for i,idx in enumerate(region.base_points):
             newi = int(i/2)
             perm_newi = invperm[newi]
+            #perm_new = underregion.permutation[newi]
             coeffset.add((regionidx,level+1,perm_newi))
 
-fig = plt.figure()
-plt.imshow(img.img,cmap=plt.cm.gray)
-ax = fig.gca()
-x = rect[1]
-y = rect[0]
-width = rect[3] - rect[1]
-height = rect[2] - rect[0]
-ax.add_patch(patches.Rectangle( (x, y),width,height,color = 'red',fill=False))
-plt.show()
+for level in range(1, img.rbepwt.levels):
+    prev_reg_len = 0
+    for regionidx,region in imb.rbepwt.region_collection_at_level[level]:
+        if regionidx not in labels:
+            prev_reg_len += len(region)
+            continue
+        wd = img.rbepwt.wavelet_details[level][prev_reg_len:prev_reg_len + len(region)]
+        for idx,value in enumerate(wd):
+            if (regionidx,level,idx) not in coeffset:
+                img.rbepwt.wavelet_details[level][prev_reg_len + idx] = 0
+        prev_reg_len += len(region)
+
+img.decode_rbepwt()
+img.show_decoded()
