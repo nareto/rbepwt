861525a272e8b5d339dd070f20e7ae6ade1edd1b
Renato <r.budinich@math.uni-goettingen.de>
2017-01-20 13:04:10 +0100

    drawing now works, started on find_region

--- scripts/roi_test.py
+++ scripts/roi_test.py
@@ -42,16 +42,18 @@ class DrawRoi:
         'connect to all the events we need'
         self.cidpress = self.fig.canvas.mpl_connect(
             'button_press_event', self.on_press)
-        self.cidrelease = self.fig.canvas.mpl_connect(
-            'button_release_event', self.on_release)
         self.cidmotion = self.fig.canvas.mpl_connect(
             'motion_notify_event', self.on_motion)
+        self.cidrelease = self.fig.canvas.mpl_connect(
+            'button_release_event', self.on_release)
 
     def disconnect(self):
         'disconnect all the stored connection ids'
         self.fig.canvas.mpl_disconnect(self.cidpress)
         self.fig.canvas.mpl_disconnect(self.cidrelease)
         self.fig.canvas.mpl_disconnect(self.cidmotion)
+        #for idx,arr in enumerate(self.roi):
+        #    print('%4d  %s' % (idx,tuple(arr)))
 
     def on_press(self, event):
         'on button press we will see if the mouse is over us and store some data'
@@ -63,47 +65,60 @@ class DrawRoi:
 
     def on_motion(self, event):
         'on motion we will move the rect if the mouse is over us'
+        if not self.press: return
         if event.inaxes != self.axes: return
-        #if self.press and (event.xdata,event.ydata) != (None,None):
         #print((int(event.xdata),int(event.ydata)))
+        point = np.array((int(event.xdata),int(event.ydata)))
+        if np.array_equal(point,self.roi[-1]): return
         if self.press:
-            point = np.array((int(event.xdata),int(event.ydata)))
-            #if (point == self.roi[-1]).all(): return
-            if np.array_equal(point,self.roi[-1]): return
-            else:
-                target_point = point
-                if is_array_in_list(point,self.roi) and len(self.roi) > 3:
+            target_point = point
+            cur_point = self.roi[-1]
+            while not np.array_equal(target_point,cur_point):
+                neigh = rbepwt.neighborhood(cur_point,1,mode='square',hole=True)
+                min_dist = np.linalg.norm(target_point-cur_point)
+                for p in neigh[1:]:
+                    p = np.array(p)
+                    d = np.linalg.norm(target_point - p)
+                    if d < min_dist:
+                        min_dist = d
+                        cur_point = p
+                if is_array_in_list(cur_point,self.roi) and len(self.roi) > 3:
                     self.disconnect()
+                    self.draw_roi()
                     return
-                cur_point = self.roi[-1]
-                print(cur_point,target_point)
-                while not np.array_equal(target_point,cur_point):
-                    neigh = rbepwt.neighborhood(cur_point,1,mode='square',hole=True)
-                    min_dist = np.linalg.norm(target_point-cur_point)
-                    for p in neigh[1:]:
-                        p = np.array(p)
-                        d = np.linalg.norm(target_point - p)
-                        if d < min_dist:
-                            min_dist = d
-                            cur_point = p
-                    self.roi.append(cur_point)
-                    if not self.draw_points:
-                        self.roi_plt.append(plt.plot([],[],'-r')[0])
-                self.draw_roi()
+                self.roi.append(cur_point)
+                if not self.draw_points:
+                    self.roi_plt.append(plt.plot([],[],'-r')[0])
+            self.draw_roi()
 
 
     def on_release(self,event):
         self.press = False
         self.disconnect()
-        for idx,arr in enumerate(self.roi):
-            print('%4d  %s' % (idx,tuple(arr)))
         
     def draw(self):
         """Opens a window where the user should draw (click+drag) a closed curve and returns the set of points in the interior"""
-        #self.axes = plt.imshow(self.array,cmap=plt.cm.gray).axes
         plt.imshow(self.array,cmap=plt.cm.gray)
+        plt.xlim((0,self.array.shape[1]))
+        plt.ylim((self.array.shape[0],0))
         plt.show()
 
+    def find_region(self):
+        border = set()
+        for arr in self.roi:
+            border.add(tuple(arr))
+        ret = np.ones_like(self.array)
+        cur_point = (0,0)
+        found = False
+        while not found:
+            neigh = rbepwt.neighborhood(cur_point,1,mode='square',hole=True)
+            for p in neigh:
+                m,n = self.array.shape
+                if p[0] >= 0 and p[0] < n and p[1] >=0 and p[1] < m and p not in border:
+                    ret[p] = 0
+            
+
+        
 def in_out_roi(percin,percout,second_image=True):
     #picklepath = '../pickled/cameraman256-easypath-haar-16levels'
     picklepath = '../pickled/house256-easypath-haar-16levels'
